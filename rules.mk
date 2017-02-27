206b9f0360915e7d0c8ca5961f5689c81a3832b0
HoangViet93 <hoangtheviet93@gmail.com>
2017-02-27 02:23:07 -0800

    add binary folder when make

--- rules.mk
+++ rules.mk
@@ -1,11 +1,11 @@
 %.bin: %.elf
-	$(OBJCOPY) -O binary $< $@ 
+	$(OBJCOPY) -O binary $(BINARY_PATH)$< $(BINARY_PATH)$@ 
 
 %.hex: %.elf
-	$(OBJCOPY) -O ihex $< $@ 
+	$(OBJCOPY) -O ihex $(BINARY_PATH)$< $(BINARY_PATH)$@ 
 
 %.elf: $(OBJS) $(LDSCRIPT)
-	$(LD) $(OBJS) $(LDFLAGS) -T$(LDSCRIPT) -o $@
+	$(LD) $(OBJS_PATH) $(LDFLAGS) -T$(LDSCRIPT) -o $(BINARY_PATH)$@
 
 %.o: %.c 
-	$(CC) $(CFLAGS) $(INCLUDE_PATH) $< -o $@
+	$(CC) $(CFLAGS) $(INCLUDE_PATH) $< -o $(BINARY_PATH)$@
